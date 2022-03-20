36a2ab1e57e42731410e84951141b7907e7d56e2
Juan Logrono <juanjoselogrono@gmail.com>
2022-03-20 12:44:06 -0300

    desafio 3

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const Contenedor = require('./controllers/products')
+const random = require('random')
+
+const app = express()
+
+const products = new Contenedor("./productos.txt")
+const PORT = 8080
+
+
+const server = app.listen(PORT, () => {
+    console.log(`puerto ${server.address().port}`)
+})
+server.on("error", error => console.log(error))
+
+
+let arrayProductos = []
+const verArray = products.getAll()
+verArray.then((R) => { arrayProductos = JSON.parse(R) })
+
+
+app.get('/productos', (req, res) => {
+    res.send(arrayProductos)
+})
+app.get('/productoRandom', (req, res) => {
+    let arrayNum = []
+    arrayProductos.forEach(element => { arrayNum = [...arrayNum, element.id] });
+    let nroRandom = random.integer(0, arrayNum.length - 1)
+    const randomProd = products.getById(arrayNum[nroRandom])
+    randomProd.then((response) => res.send(response))
+    
+});
+
